3c1c6a964f3d4b4f11ac0ead0008ebac2c267aec
Krishna Sundarram <krishna.sundarram@gmail.com>
2014-04-04 09:06:23 +0530

    Add ability to specify params on command line

--- goof.go
+++ goof.go
@@ -1,19 +1,43 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
+)
+
+var (
+	filename = flag.String("f", "", "The name of the file to be shared")
+	n        = flag.Int("n", 1, "The number of times the file should be shared")
+	t        = flag.Int("t", 0, "Server timeout")
 )
 
 type fileHandler struct {
 	filename string
-	times    int
+	n        int
 }
 
-func (f fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (f *fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	f.n = f.n - 1
+	if f.n == -1 {
+		log.Fatal("Finished serving. Server exiting.")
+	}
+	log.Println(f.filename)
 	http.ServeFile(w, r, f.filename)
 }
 
+func exitafter(minutes int) {
+	delay := fmt.Sprintf("%dm", minutes)
+	duration, _ := time.ParseDuration(delay)
+	<-time.After(duration)
+	log.Fatal("Server timed out.")
+}
+
 func main() {
-	handler := fileHandler{"/home/nindalf/Pictures/wallpapers/octocats/baracktocat.jpg", 1}
-	http.ListenAndServe(":8086", handler)
+	flag.Parse()
+	go exitafter(*t)
+	handler := fileHandler{"/home/nindalf/Pictures/wallpapers/octocats/chellocat.jpg", *n}
+	http.ListenAndServe(":8086", &handler)
 }
